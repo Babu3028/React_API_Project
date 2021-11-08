a63ef6afe10dedae36dbc32512ab05a19f101acc
Babu3028 <92800993+Babu3028@users.noreply.github.com>
2021-11-08 23:10:33 +0530

    Code

---
+++ src/components/ResourceList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
+const ResourceList = ({resourceName}) =>{
+    const [resources , setResources] = useState([])
+
+    const fetchResources =  async(resourceName) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resourceName}`);
+        setResources(response.data)
+    }
+
+    useEffect(()=>{
+         fetchResources(resourceName);
+    },[resourceName])
+
+    return(
+        <ul>
+           {resources.map(resource=>(
+               <li key={resource.id}>{resource.title}</li>
+           ))} 
+        </ul>
+    )
+}
+
+export default ResourceList
